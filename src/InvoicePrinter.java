19e839655466112956b23d7fd91e4064b7d6cbe6
javisamlobo <javisamlobo@gmail.com>
2022-11-24 18:45:54 +0100

    agregado la funcion de imprimir factura

--- src/InvoicePrinter.java
+++ src/InvoicePrinter.java
@@ -1,6 +1,8 @@
 public class InvoicePrinter {
     public void print(Invoice invoice){
-    System.out.print("Factura: " + invoice.getCode() + " | " + invoice.getSale() );
-
+    System.out.println("Factura: " + invoice.getCode() + " | " + invoice.getDate() );
+    System.out.println("Cliente: " + invoice.getClient().getId() + " | " + invoice.getClient().getName() + " | " + invoice.getClient().getTown() + " | " + invoice.getClient().getProvince() + " | " + invoice.getClient().getEmail());
+    System.out.println("Linea venta (producto / servicio): " + invoice.getSale().getCode() + " | " + invoice.getSale().getname() + " | " + invoice.getSale().getPrice()+ " | " + invoice.getSale().getIva() + " | " + invoice.getTotal());
+    System.out.println("Total: " + invoice.getTotal());
     }
 }
